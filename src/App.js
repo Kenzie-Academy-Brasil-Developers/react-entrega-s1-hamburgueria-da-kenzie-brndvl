21bdadcbd10fb3621d3bce10dd94fe08dd884f99
Bruno Dela Val Assis <bc.delaval@gmail.com>
2021-10-18 12:08:53 -0300

    duplicate itens

--- src/App.js
+++ src/App.js
@@ -1,67 +1,62 @@
-import './App.css';
-import {useState} from "react"
-import {MenuContainer} from "./components/MenuContainer"
-import {Products} from "./components/Products"
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import { MenuContainer } from "./components/MenuContainer";
 
 function App() {
-
   const [products, setProducts] = useState([
-    { id: 1, name: 'Hamburguer', category: 'Sanduíches', price: 7.99 },
-    { id: 2, name: 'X-Burguer', category: 'Sanduíches', price: 8.99 },
-    { id: 3, name: 'X-Salada', category: 'Sanduíches', price: 10.99 },
-    { id: 4, name: 'Big Kenzie', category: 'Sanduíches', price: 16.99 },
-    { id: 5, name: 'Guaraná', category: 'Bebidas', price: 4.99 },
-    { id: 6, name: 'Coca', category: 'Bebidas', price: 4.99 },
-    { id: 7, name: 'Fanta', category: 'Bebidas', price: 4.99 },
-  ]); 
+    { id: 1, name: "Hamburguer", category: "Sanduíches", price: 7.99 },
+    { id: 2, name: "X-Burguer", category: "Sanduíches", price: 8.99 },
+    { id: 3, name: "X-Salada", category: "Sanduíches", price: 10.99 },
+    { id: 4, name: "Big Kenzie", category: "Sanduíches", price: 16.99 },
+    { id: 5, name: "Guaraná", category: "Bebidas", price: 4.99 },
+    { id: 6, name: "Coca", category: "Bebidas", price: 4.99 },
+    { id: 7, name: "Fanta", category: "Bebidas", price: 4.99 },
+  ]);
 
   const [filteredProduct, setFilteredProduct] = useState("");
 
-  const [currentSale, setCurrentSale] = useState([])
+  const [currentSale, setCurrentSale] = useState([]);
 
-  const [cartTotal, setCartTotal] = useState(0)
+  const [cartTotal, setCartTotal] = useState(0);
 
-//functions
+  //functions
 
-  const showProducts = products.filter((item) => item.name.toLowerCase().includes(filteredProduct.toLowerCase()))
-    
+  const showProducts = products.filter((item) =>
+    item.name.toLowerCase().includes(filteredProduct.toLowerCase())
+  );
 
-  const total = () => {
-    if (currentSale.length !== 0) {
-      setCartTotal(currentSale.reduce((acc, p) => acc + p.price, 0))
+  const total = currentSale.reduce((acc, p) => p.price + acc, 0);
+
+  const handleClick = (productId) => {
+    const newArr = products.filter((item) => item.id === productId);
+    if(currentSale.length === 0){
+      setCurrentSale([...currentSale, newArr[0]]);
+
+    } else if (currentSale.includes(newArr) === false){
+
+      setCurrentSale([...currentSale, newArr]);
     }
-  }
 
-  const handleClick = (productId) => { 
-    const newArr = products.filter((item) => item.id === productId) 
-    setCurrentSale([...currentSale, (newArr[0])])
-    total()
-  }
-
+    console.log(newArr[0].id)
+  };
 
   return (
     <div className="App">
       <h1>Hamburgueria da Kenzie</h1>
 
-
       <div className="App-header">
-
-        
-
-      <MenuContainer products={products} 
-      handleClick={handleClick} 
-      showProducts={showProducts} 
-      setFilteredProduct={setFilteredProduct} 
-      filteredProduct={filteredProduct}
-      currentSale={currentSale}
-      cartTotal={cartTotal}
-      />
-
-      
-
+        <MenuContainer
+          products={products}
+          handleClick={handleClick}
+          showProducts={showProducts}
+          setFilteredProduct={setFilteredProduct}
+          filteredProduct={filteredProduct}
+          currentSale={currentSale}
+          cartTotal={cartTotal}
+          total={total}
+        />
       </div>
-    
-
     </div>
   );
 }
--- src/components/MenuContainer/index.js
+++ src/components/MenuContainer/index.js
@@ -1,48 +1,58 @@
-import { Products } from "../Products"
-import "./style.css"
+import { Products } from "../Products";
+import "./style.css";
 
-export const MenuContainer = ({products, handleClick, filteredProduct, showProducts, setFilteredProduct, currentSale, cartTotal}) => {
+export const MenuContainer = ({
+  handleClick,
+  filteredProduct,
+  showProducts,
+  setFilteredProduct,
+  currentSale,
+  cartTotal,
+  total
+}) => {
 
+  console.log(showProducts) 
+  return (
+    <div className="container">
+      <input
+        type="text"
+        value={filteredProduct}
+        onChange={(e) => setFilteredProduct(e.target.value)}
+      />
 
+      <ul>
+        {showProducts.map((item, index) => (
+          <li key={index}>
+            <Products
+              id={item.id}
+              name={item.name}
+              category={item.category}
+              price={item.price}
+              handleClick={handleClick}
+              buttonAppear={true}
+            />
+          </li>
+        ))}
+      </ul>
 
-    return(
-        <div className="container">
-        <input 
-            type="text" 
-            value={filteredProduct}
-            onChange={(e) => setFilteredProduct(e.target.value)}/>
-            
+      <span className="subtotal">Sub-total: R${total.toFixed(2)}</span>
+
+      {
         <ul>
-            
-            {showProducts.map((item, index) => (
-               <li>
-                   <Products key={index} 
-                   id={item.id} 
-                   name={item.name} 
-                   category={item.category} 
-                   price={item.price} 
-                   handleClick={handleClick}
-                   buttonAppear={true}
-                   />
-               </li> 
-
-            ))}
-           
+          {currentSale.map((item, index) => (
+            <li>
+              <Products
+                key={index}
+                id={item.id}
+                name={item.name}
+                category={item.category}
+                price={item.price}
+                buttonAppear={false}
+              />
+            </li>
+          ))}
         </ul>
-        
-        <span className="subtotal">Sub-total: R${cartTotal.toFixed(2)}</span>
-
-      {<ul>
-            
-            {currentSale.map((item, index) => (
-               <li>
-                   <Products key={index} id={item.id} name={item.name} category={item.category} price={item.price} buttonAppear={false}/>
-               </li> 
-
-            ))}
-           
-        </ul>}
-        
-        </div>
-    )
-}
+      }
+    </div>
+  );
+};
